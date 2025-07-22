e4561aa57dbd29de53a6f7c37ed7c6d6586bbc71
Jeremiah Joseph <jerrycul2001@gmail.com>
2025-07-22 22:39:15 +0100

    feat: add component for screen headers

---
+++ lib/components/ScreenHeader.tsx
@@ -0,0 +1,20 @@
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Box } from "./ui/box";
+import { ChevronLeftIcon, Icon } from "./ui/icon";
+import { Pressable } from "./ui/pressable";
+
+export default function ScreenHeader({ navigation }: { navigation: any }) {
+  const { top } = useSafeAreaInsets();
+
+  return (
+    <Box style={{ paddingTop: top }}>
+      <Pressable onPress={() => navigation.canGoBack() && navigation.goBack()}>
+        <Icon
+          as={ChevronLeftIcon}
+          className="text-typography-900 m-2 w-10 h-10"
+          fill="none"
+        />
+      </Pressable>
+    </Box>
+  );
+}
