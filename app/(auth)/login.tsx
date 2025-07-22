7cb916caa732f1c54fb955dbdcbfddef3c7ea67d
David Babatunde <reol0624@gmai.com>
2025-07-22 16:06:35 +0100

    add login screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,14 +1,19 @@
+import { router } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useAuthStore } from "../../lib/store/useAuthStore";
 
 export default function LoginScreen() {
   const { login } = useAuthStore();
+  // const { signup } = useAuthStore();
 
   return (
-    <View style={styles.container}>
+    <View className="flex gap-3" style={styles.container}>
       <Text style={styles.title}>Welcome</Text>
       <TouchableOpacity style={styles.button} onPress={login}>
         <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => router.push("/(auth)/signup")}>
+        <Text style={styles.buttonText}>Sign Up</Text>
       </TouchableOpacity>
     </View>
   );
