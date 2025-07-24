b61667bc466d9615b16018e4a93e71c357470455
David Babatunde <reol0624@gmai.com>
2025-07-24 15:29:11 +0100

    fix: updated header styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,8 +22,10 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: true,
+        headerShadowVisible: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
+        headerTitleAlign: "center",
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
