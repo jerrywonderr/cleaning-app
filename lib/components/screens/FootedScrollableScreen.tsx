11604efe13cb36119aca8ef78d58301f1c8179a3
Jeremiah Joseph <jerrycul2001@gmail.com>
2025-08-16 02:35:33 +0100

    feat: add another variation of scroll screen with footer

---
+++ lib/components/screens/FootedScrollableScreen.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils/style";
+import { ClassValue } from "clsx";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Box } from "../ui/box";
+
+export default function FootedScrollableScreen({
+  children,
+  addTopInset = true,
+  addBottomInset = true,
+  keyboardVerticalOffset = Platform.OS === "ios" ? 48 : 0,
+  contentContainerClassName,
+  footer,
+}: {
+  children: React.ReactNode;
+  addTopInset?: boolean;
+  addBottomInset?: boolean;
+  keyboardVerticalOffset?: number;
+  contentContainerClassName?: ClassValue;
+  footer: React.ReactNode;
+}) {
+  const { top, bottom } = useSafeAreaInsets();
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1, backgroundColor: "white" }}
+      // keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <Box
+        className={cn("px-4", contentContainerClassName)}
+        style={{
+          flex: 1,
+          paddingTop: addTopInset ? top : 0,
+          paddingBottom: addBottomInset ? bottom : 0,
+        }}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+          {children}
+        </ScrollView>
+        <Box>{footer}</Box>
+      </Box>
+    </KeyboardAvoidingView>
+  );
+}
